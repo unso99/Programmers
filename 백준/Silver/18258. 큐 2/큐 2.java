b335b90b3f190a243edba9c22e7d0d57dac2adb7
unso99 <94777814+unso99@users.noreply.github.com>
2023-05-09 17:35:53 +0900

    [Silver IV] Title: 큐 2, Time: 1300 ms, Memory: 331452 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,85 +1,93 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	static int[] q = new int[2000000];	// 명령의 수는 2,000,000을 안넘음 
-	
-	static int size = 0;	
-	static int front = 0;
-	static int back = 0;
-	
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		
- 
-		while(N-- > 0) {
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			switch(st.nextToken()){
-			case "push": push(Integer.parseInt(st.nextToken())); break;
-			case "pop" : pop(); break;
-			case "size" : size(); break;
-			case "empty" : empty(); break;
-			case "front" : front(); break;
-			case "back" : back(); break;
-			
-			}
-		}
-		System.out.println(sb);
-	}
-	
-	static void push(int n) {
-		q[back] = n;
-		back++;
-		size++;
-	}
-	
-	static void pop() {
-		if(size == 0) {
-			sb.append(-1).append('\n');
-		}
-		else {
-			sb.append(q[front]).append('\n');	// 맨 앞의 원소를 출력 
-			size--;
-			front++;	// front가 가리키는 위치 1 증가 
-		}
-	}
-	
-	static void size() {
-		sb.append(size).append('\n');
-	}
-	
-	static void empty() {
-		if(size == 0) {
-			sb.append(1).append('\n');
-		}
-		else sb.append(0).append('\n');
-	}
-	
-	static void front() {
-		if(size == 0) { 
-			sb.append(-1).append('\n');
-		}
-		else {
-			sb.append(q[front]).append('\n');	 // 맨 앞의 원소 출력 
-		}
-	}
-	
-	static void back() {
-		if(size == 0) {
-			sb.append(-1).append('\n');
-		}
-		else {
-			sb.append(q[back - 1]).append('\n');	// 맨 뒤의 원소 출력 
-		}
-	}
- 
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int n = Integer.parseInt(br.readLine());
+        Queue queue = new Queue();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            switch(st.nextToken()){
+                case "push":
+                    queue.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    queue.pop();
+                    break;
+                case "size":
+                    queue.size();
+                    break;
+                case "empty":
+                    queue.empty();
+                    break;
+                case "front":
+                    queue.front();
+                    break;
+                case "back":
+                    queue.back();
+                    break;
+            }
+        }
+        System.out.println(sb);
+
+    }
+
+    static class Queue{
+
+        static int[] queue = new int[2000000];
+        static int first = 0;
+        static int last = 0;
+        public Queue(){}
+
+        public static void push(int x) {
+            queue[last] = x;
+            last++;
+        }
+
+        public static void pop() throws IOException {
+            if (last - first == 0) {
+                sb.append(-1).append("\n");
+            }else {
+                int q = queue[first];
+                first++;
+                sb.append(q).append("\n");
+            }
+        }
+
+        public static void size() throws IOException {
+            sb.append(last - first).append("\n");
+        }
+
+        public static void empty() throws IOException {
+            if (last - first == 0) {
+                sb.append(1).append("\n");
+            }else {
+                sb.append(0).append("\n");
+            }
+        }
+
+        public static void front() throws IOException {
+            if (last - first == 0) {
+                sb.append(-1).append("\n");
+            }else {
+                int q = queue[first];
+                sb.append(q).append("\n");
+            }
+        }
+
+        public static void  back() throws IOException {
+            if (last - first == 0){
+                sb.append(-1).append("\n");
+            }else {
+                int q = queue[last-1];
+                sb.append(q).append("\n");
+            }
+        }
+    }
 }
