91be21877a57a60b1d25bcb47d99f005af154b9e
unso99 <94777814+unso99@users.noreply.github.com>
2023-12-19 00:15:30 +0900

    [level 2] Title: 프로세스, Time: 6.66 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.kt
+++ 프로그래머스/2/42587. 프로세스/프로세스.kt
@@ -2,30 +2,31 @@ import java.util.*
 
 class Solution {
     fun solution(priorities: IntArray, location: Int): Int {
-    var answer = 0
-    val deq = ArrayDeque<Process>()
-    val target = Process(priorities[location], location)
-
-    priorities.forEachIndexed { index, i ->
-        deq.add(Process(i, index))
-    }
-
-    while (deq.isNotEmpty()) {
-        val first = deq.first()
-        if (deq.count { it.priority > first.priority } != 0) {
-            deq.addLast(first)
-            deq.removeFirst()
-        } else {
-            if (first == target) break
-            else {
+        var answer = 0
+        val target = Process(priorities[location], location)
+        val deq = ArrayDeque<Process>()
+        
+        priorities.forEachIndexed { index, value ->
+            deq.add(Process(value, index))
+        }
+        
+        while(deq.isNotEmpty()){
+            val first = deq.first()
+            
+            if(deq.count {it.priority > first.priority} != 0){
+                deq.addLast(first)
                 deq.removeFirst()
-                answer++
+            }else{
+                if(first == target) break
+                else {
+                    deq.removeFirst()
+                    answer++
+                }
             }
         }
+
+        return answer + 1
     }
-
-    return answer + 1
-}
-
-data class Process(val priority: Int, val location: Int)
+    
+    data class Process(val priority : Int, val location : Int)
 }
