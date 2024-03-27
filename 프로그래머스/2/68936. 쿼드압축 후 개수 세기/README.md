c343649393a9ab2e0ec47607ecaff875a18cb6a1
unso99 <94777814+unso99@users.noreply.github.com>
2024-03-27 10:00:55 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 4.70 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 쿼드압축 후 개수 세기 - 68936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936) 
 
 ### 성능 요약
 
-메모리: 71 MB, 시간: 3.69 ms
+메모리: 71.1 MB, 시간: 4.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 26일 23:48:17
+2024년 03월 27일 10:00:52
 
 ### 문제 설명
 
--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.kt
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.kt
@@ -1,25 +1,26 @@
 class Solution {
     fun solution(arr: Array<IntArray>): IntArray {
-        val zeroOne = intArrayOf(0,0)
+        var answer: IntArray = intArrayOf(0,0)
         fun dfs(x : Int, y : Int, size : Int) {
             val isZipable = canZip(arr, x, y, size)
             if(isZipable != -1) {
-                zeroOne[isZipable]++
+                answer[isZipable] += 1
                 return
-            } 
+            }
             
-            dfs(x,y,size/2)
-            dfs(x,y + size/2,size/2)
-            dfs(x + size/2 ,y,size/2)
-            dfs(x + size/2 ,y + size/2,size/2)
+            dfs(x,y,size / 2)
+            dfs(x,y + size / 2,size / 2)
+            dfs(x + size / 2,y,size / 2)
+            dfs(x + size / 2,y + size / 2,size / 2)
+            
         }
         dfs(0,0,arr.size)
-        return zeroOne
+        return answer
     }
     
-    fun canZip(arr: Array<IntArray>, x : Int, y : Int, size : Int) : Int {
+    fun canZip(arr : Array<IntArray>, x : Int, y : Int, size : Int) : Int {
         val start = arr[x][y]
-        for(i in x until x + size) {
+        for (i in x until x +size) {
             for(j in y until y + size) {
                 if(arr[i][j] != start) return -1
             }
