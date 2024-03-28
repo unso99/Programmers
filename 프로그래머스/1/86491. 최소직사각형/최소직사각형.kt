9cd7ee1cbf6ce386acbad577ca0b6d41fbdb7644
unso99 <94777814+unso99@users.noreply.github.com>
2024-03-28 23:26:11 +0900

    [level 1] Title: 최소직사각형, Time: 0.76 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -1,65 +1,19 @@
 class Solution {
     fun solution(sizes: Array<IntArray>): Int {
-        var answer: Int
-
-        var bigIndex1 = -1
-        var bigIndex2 = -1
-        var maxValue = -1
-        var secondValue = -1
-
-        sizes.forEachIndexed { index, arr ->
-            if (arr[0] > maxValue) {
-                bigIndex1 = index
-                bigIndex2 = 0
-                maxValue = arr[0]
-            }
-            if (arr[1] > maxValue) {
-                bigIndex1 = index
-                bigIndex2 = 1
-                maxValue = arr[1]
-            }
-        }
-        sizes.forEachIndexed { index, arr ->
-            when (bigIndex2) {
-                0 -> {
-                    if (index != bigIndex1) {
-                        var temp0 = arr[0]
-                        var temp1 = arr[1]
-
-                        if (temp0 < temp1) {
-                            arr[0] = temp1
-                            arr[1] = temp0
-                        }
-                    }
-                }
-
-                else -> {
-                    if (index != bigIndex1) {
-                        var temp0 = arr[0]
-                        var temp1 = arr[1]
-
-                        if (temp0 > temp1) {
-                            arr[0] = temp1
-                            arr[1] = temp0
-                        }
-                    }
-                }
-            }
-        }
-
+        var answer: Int = 0
+        var max = 1
+        var min = 1
         sizes.forEach {
-            if (bigIndex2 == 0) {
-                if (secondValue < it[1]) secondValue = it[1]
-            } else {
-                if (secondValue < it[0]) secondValue = it[0]
-            }
+            val a = if(it[0] >= it[1] ) it[0]
+            else it[1]
+            val b = if(it[0] >= it[1] ) it[1]
+            else it[0]
 
+            if(max <= a) max = a
+            if(min <= b) min = b
         }
 
-        maxValue = sizes[bigIndex1][bigIndex2]
-
-        answer = maxValue * secondValue
-
+        answer = max * min
 
         return answer
     }
