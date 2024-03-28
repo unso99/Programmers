7572e1ec25e7eb6a155d4ba501549be3fc1f5153
unso99 <94777814+unso99@users.noreply.github.com>
2024-03-28 10:21:26 +0900

    [level 2] Title: 의상, Time: 8.06 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.kt
+++ 프로그래머스/2/42578. 의상/의상.kt
@@ -1,19 +1,10 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
         var answer = 1
-        val clothMap = clothes.groupBy { it[1] }.mapValues { it.value.map { it[0] }.toMutableList() }
+        val clothMap = clothes.groupBy{it[1]}.mapValues{it.value.map{it[0]}.toMutableList()}
         clothMap.forEach {
-            println(it)
-            answer *= it.value.size+1
+            answer *= it.value.size + 1
         }
-
-
         return answer -1
     }
-    
-    private fun combination(n: Int, r: Int): Int {
-        if (n == r || r == 0) return 1
-        else return combination(n - 1, r - 1) + combination(n - 1, r)
-    } 
-    
 }
