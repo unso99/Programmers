46552687a3e87b96cc7b20404c3b6e3f20d5d2b6
unso99 <94777814+unso99@users.noreply.github.com>
2024-03-31 21:15:01 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 19.85 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        return arr.map {
+            if (it >= 50 && it % 2 == 0) it / 2
+            else if (it < 50 && it % 2 != 0) it * 2
+            else it
+        }.toIntArray()
+    }
+}
