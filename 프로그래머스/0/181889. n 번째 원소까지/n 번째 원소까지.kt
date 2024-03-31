a5516066e13ca4233ceecd9bc3aa8fcbcbaedebc
unso99 <94777814+unso99@users.noreply.github.com>
2024-03-31 21:11:14 +0900

    [level 0] Title: n 번째 원소까지, Time: 5.87 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(num_list: IntArray, n: Int): IntArray {
+        val list = mutableListOf<Int>()
+        
+        for(i in 0 until n) {
+            list.add(num_list[i])
+        }
+        
+        return list.toIntArray()
+    }
+}
