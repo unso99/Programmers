1ad29913d10af8038bb1ca0a69dfc77c34d75ffa
unso99 <94777814+unso99@users.noreply.github.com>
2024-04-11 12:12:06 +0900

    [level 0] Title: 모스부호 (1), Time: 11.25 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(letter : String): String {
+        val morse = mapOf(".-" to "a","-..." to "b","-.-." to "c","-.." to "d","." to "e","..-." to "f",
+        "--." to "g","...." to "h",".." to "i",".---" to "j","-.-" to "k",".-.." to "l",
+        "--" to "m","-." to "n","---" to "o",".--." to "p","--.-" to "q",".-." to "r",
+        "..." to "s","-" to "t","..-" to "u","...-" to "v",".--" to "w","-..-" to "x",
+        "-.--" to "y","--.." to "z")
+
+        val arr = letter.split(" ")
+        var result = ""
+        arr.forEach {
+            result += morse[it]
+        }
+        return result
+    }
+}
